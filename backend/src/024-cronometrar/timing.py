629249cb8561418cfb924aefb9257796ab2f2a9b
mondeja <mondejar1994@gmail.com>
2018-01-13 14:10:19 +0100

    Minor improvements in some files [ci skip]

--- backend/src/024-cronometrar/timing.py
+++ backend/src/024-cronometrar/timing.py
@@ -9,7 +9,7 @@ def fn_timer(miliseconds=False):
     from time import time
     from functools import wraps
     from decimal import Decimal
-    def wrap_args(func): # Envoltura para insertar el 
+    def wrap_args(func): # Envoltura para insertar el
         @wraps(func)    # argumento miliseconds en el decorador
         def function_timer(*args, **kwargs):
             start_time = time()
@@ -36,74 +36,119 @@ def slow_function():
     for _ in range(50000):
         _ += 1
 
+# Función para demostrar el uso de timeit con la consola IPython
+def slow_function_2():
+    for _ in range(50000):
+        _ += 1
+
 # -----------------------------------------------
 
 # Existen varias formas de cronometrar:
 
 if __name__ == "__main__":
-    # ================================
-    
+    # ============================================================================
+
     # Opción 1 -> Decorador
     slow_function()
-    
-    # ================================
-    
+
+    # ============================================================================
+
     # Opción 2 -> Módulo timeit
     from timeit import timeit
-    
+
     tiempo_que_tarda = timeit("[ _ for _ in range(50000) ]", # Ejecución a realizar
                               setup="pass", # Imports e inicializaciones
                               number=5) # Número de repeticiones (devuelve la media)
-    print("Tiempo total:", Decimal(tiempo_que_tarda))  
-    
+    print("Tiempo total:", Decimal(tiempo_que_tarda))
+
+    """
+    Con IPython, el cronometrado con timeit es más fácil.
+    Podemos abrir la consola ipython, importar el módulo donde
+    se encuentran nuestras funciones y ejecutar
+    %timeit funcion()
+
+    -----------------------------------------------------------------------
+    Python 3.5.3 (default, Jan 19 2017, 14:11:04)
+    Type 'copyright', 'credits' or 'license' for more information
+    IPython 6.2.1 -- An enhanced Interactive Python. Type '?' for help.
+
+    In [1]: import timing
+
+    In [2]: %timeit timing.slow_function_2()
+    2.03 ms ± 2.47 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
+    -----------------------------------------------------------------------
+
+    Más información:
+    http://ipython.readthedocs.io/en/stable/interactive/magics.html#magic-timeit
+    """
+
     """
     Podemos llamarlo también como un script
     python3 -m timeit -n <numero_de_repeticiones> -r <media_de_tiempo_por_test> -s <"setup"> <"ejecución">
     """
-    
-    # ================================
-    
+
+    # =========================================================================
+
     # Opción 3 -> El comando time de Unix
     """
     time -p python3 measure.py
-    
+
     Salida:
         real 0.08
         user 0.03
         sys 0.02
-    
+
     - real indica el tiempo total ejecutando el script
     - user indica la cantidad de tiempo que la CPU gasta ejecutando el script
-    - sys indica indica la cantidad de tiempo gastado en funciones a nivel de núcleo 
+    - sys indica indica la cantidad de tiempo gastado en funciones a nivel de núcleo
     """
-    
-    # ================================
-    
+
+    """
+    IPython también tiene una implementación de time con %time:
+
+    -------------------------------------------------------------------
+    Python 3.5.3 (default, Jan 19 2017, 14:11:04)
+    Type 'copyright', 'credits' or 'license' for more information
+    IPython 6.2.1 -- An enhanced Interactive Python. Type '?' for help.
+
+    In [1]: import timing
+
+    In [2]: %time timing.slow_function_2()
+    CPU times: user 8 ms, sys: 0 ns, total: 8 ms
+    Wall time: 8.84 ms
+    -------------------------------------------------------------------
+
+    Más información:
+    http://ipython.readthedocs.io/en/stable/interactive/magics.html#magic-time
+    """
+
+    # =========================================================================
+
     # Opción 4 -> Módulo cProfile
     """
     python3 -m cProfile -s cumulative measure.py
-    
+
     Este comando te indicará cuánto tiempo toma el script
     en cada función, así como el número de llamadas y otra
-    información, de una forma más detallada    
+    información, de una forma más detallada
     """
-    
-    # ================================
-    
+
+    # =========================================================================
+
     # Opción 5 -> Módulo line_profiler
     """
     Este módulo es útil si necesitas comprobar cuánto tiempo
     gasta la CPU en cada línea de código. Primero hay que instalarlo:
-    
+
     pip3 install line_profiler
-    
+
     Para usarlo hay que indicar las funciones que queremos medir
     con un decorador:  @profile
-    
+
     Luego debemos llamar al script con el siguiente comando:
-    
+
     kernprof -l -v measure.py
-    
+
     La salida es la siguiente:
         Timer unit: 1e-06 s
 
@@ -119,7 +164,9 @@ if __name__ == "__main__":
             31     50001        36101      0.7     48.7      for _ in range(50000):
             32     50000        38052      0.8     51.3          _ += 1
     """
-    
+    # =========================================================================
+
+
 """
 Fuente:
 http://www.marinamele.com/7-tips-to-time-python-scripts-and-control-memory-and-cpu-usage
--- backend/src/029-concurrencia/procesos/basico/python/_proc.py
+++ backend/src/029-concurrencia/procesos/basico/python/_proc.py
@@ -25,16 +25,35 @@ def hola(nombre):
 # Creación de un proceso con argumentos
 proceso = Process(target=hola, args=("Paco",))
 
-# Lanzar un proceso 
+# Lanzar un proceso
 proceso.start()
 
 # Esperar a que termine un proceso
 proceso.join()
 
+
+print()
 # ==============================================
 
+"""Podemos crear un proceso como una clase"""
+
+class Proceso(Process):
+    def __init__(self, arg1):
+        super(Proceso, self).__init__()
+        self.arg1 = arg1
+
+    # Dentro de esta método irá el código que ejecutará
+    def run(self):
+        msg_schema = "Hola soy un proceso desde clase con argumento %r, my PID es %d"
+        print(msg_schema % (self.arg1, os.getpid()))
+
+p = Proceso(True)
+p.start()
+p.join()
+
+
 """
-Fuente:
+Fuentes:
 https://docs.python.org/3/library/multiprocessing.html
 """
 
