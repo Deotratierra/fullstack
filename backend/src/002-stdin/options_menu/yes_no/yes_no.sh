ce1ec02f2bd49d3892f0263bb17122078a0c4749
mondeja <mondejar1994@gmail.com>
2018-01-18 11:23:38 +0100

    Some examples updated [ci skip]

--- backend/src/002-stdin/options_menu/yes_no/yes_no.sh
+++ backend/src/002-stdin/options_menu/yes_no/yes_no.sh
@@ -1,21 +1,68 @@
 #!/bin/bash
 
-INVALIDA=1
-while [ $INVALIDA -eq 1 ]; do
-  read -p "¿Quieres instalar el programa? S/N: " -n 1 -r
-  echo
-  if [[ $REPLY =~ ^[Ss]$ ]]; then
-    echo "Instalando..."
-    INVALIDA=0
-  elif [[ $REPLY =~ ^[Nn]$ ]]; then
-    echo "Lo dejaremos sin instalar."
-    INVALIDA=0
-  else
-    echo "Opción inválida"
-    echo
-  fi
-done
+# ===============================================
 
+# Función de selección sí o no
+# Toma una pregunta como primer argumento
+# Guarda la respuesta (S/N) en la variable $SELEC
+function selec_si_no() {
+  INVALIDA=1
+  while [ $INVALIDA -eq 1 ]; do
+    INVALIDA=0
+    read -p "$1" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Ss]$ ]]; then    # SI
+      SELEC="S"
+    elif [[ $REPLY =~ ^[Nn]$ ]]; then  # NO
+      SELEC="N"
+    else
+      echo "Opción inválida"
+      echo
+      INVALIDA=1
+    fi
+  done
+}
+
+selec_si_no "¿Quieres instalar el programa? S/N: "
+
+if [[ $SELEC == "S" ]]; then
+  echo "Instalando..."
+else
+  echo "Abortando instalación..."
+fi
+
+
+# ===============================================
+# In english for easy introduction in you projects
+
+# Selection function yes/no
+# Pass a question as first argument
+# Then check the answer (Y/N) on $SELEC variable
+function select_yes_no() {
+  invalid=1
+  while [ $invalid -eq 1 ]; do
+    invalid=0
+    read -p "$1" -n 1 -r
+    echo
+    if [[ $REPLY =~ ^[Yy]$ ]]; then    # YES
+      SELEC="Y"
+    elif [[ $REPLY =~ ^[Nn]$ ]]; then  # NO
+      SELEC="N"
+    else
+      echo "Invalid option"
+      echo
+      invalid=1
+    fi
+  done
+}
+
+selec_si_no "Do you want to install the program? Y/N: "
+
+if [[ $SELEC == "Y" ]]; then
+  echo "Installing..."
+else
+  echo "Aborting install..."
+fi
 
 # ===============================================
 
---
+++ backend/src/046-sistema_operativo/distrib/distrib.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+# Obtener información sobre la versión de la distribución
+#cat /etc/os-release
+
+#echo
+
+
+# Obtener el número de la versión de la distibución
+# (válido en: {Ubuntu, Debian, CentOs})
+# Obtén el nombre de la distribución en la variable $DIST
+# y la versión de la misma en la variable $DIST_VERSION
+function osdist_version() {
+
+  DIST=$(grep -e '^ID=' /etc/os-release | cut -d'=' -f2)
+
+  # Dependiendo de la versión, el comando para saber su número varía
+  case $DIST in
+  debian)
+    DIST_VERSION=$(grep -e '.*' /etc/debian_version)
+    ;;
+  ubuntu)
+    DIST_VERSION=$(grep DISTRIB_RELEASE /etc/lsb-release | cut -d = -f 2)
+    ;;
+  '"centos"')
+    DIST_VERSION=$(cat /etc/centos-release | cut -d " " -f 4)
+    ;;
+  esac
+
+}
+
+osdist_version
+echo $DIST $DIST_VERSION
